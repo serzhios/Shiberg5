5eea0408a26d4774408f7476317d0252148cfcfa
Sergey Rumyancev <studiors39@gmail.com>
2024-11-28 23:51:37 +0200

    главная страница опрос

--- src/js/module/sliderMain3.js
+++ src/js/module/sliderMain3.js
@@ -1,14 +1,14 @@
-export function sliderMain3(){
-    if(document.querySelectorAll(".section6__swiper")){
-        var swiper = new Swiper(".section6__swiper", {
-            slidesPerView: 1,
-            spaceBetween: 30,
-            loop: true,
-            navigation: {
-              nextEl: ".swiper-button-next ",
-              prevEl: ".swiper-button-prev",
+// export function sliderMain3(){
+//     if(document.querySelectorAll(".section6__swiper")){
+//         var swiper = new Swiper(".section6__swiper", {
+//             slidesPerView: 1,
+//             spaceBetween: 30,
+//             loop: true,
+//             navigation: {
+//               nextEl: ".swiper-button-next ",
+//               prevEl: ".swiper-button-prev",
 
-            },
-          });
-}
-}
+//             },
+//           });
+// }
+// }
