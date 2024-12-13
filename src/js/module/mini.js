e16951d62a9b17287ffe23160aa0dca4b923d8c0
Jkrass210 <jkrass210@gmail.com>
2024-12-13 15:52:48 +0300

    new_push

--- src/js/module/mini.js
+++ src/js/module/mini.js
@@ -1,25 +1,16 @@
-export function Mini(){
-    if(document.querySelectorAll(".section6__swiper")){
+export function Mini() {
+  if (document.querySelectorAll(".section6__swiper")) {
+    const buttons = document.querySelectorAll(".section6__btn");
+    const swipers = document.querySelectorAll(".section6__swiper");
 
-
-        const buttons = document.querySelectorAll(".section6__btn");
-        const swipers = document.querySelectorAll(".section6__swiper");
-        // const marker = document.querySelectorAll(".section6__marker");
-        // const inner = document.querySelectorAll(".inner-square");
-
-        buttons.forEach((button, index) => {
-          button.addEventListener("click", () => {
-            buttons.forEach(btn => btn.classList.remove("active"));
-            // marker.forEach(btn => btn.classList.remove("active"));
-            // inner.forEach(btn => btn.classList.remove("active"));
-            swipers.forEach(swiper => swiper.classList.remove("active"));
-            button.classList.add("active");
-            // marker.classList.add("active");
-            // inner.classList.add("active");
-            swipers[index].classList.add("active");
-          });
-        });
-
-    }
+    buttons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        buttons.forEach(btn => btn.classList.remove("active"));
+        swipers.forEach(swiper => swiper.classList.remove("active"));
+        button.classList.add("active");
+        swipers[index].classList.add("active");
+      });
+    });
 
   }
+}
